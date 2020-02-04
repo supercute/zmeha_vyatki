c284268807d564850c2cab66fddbbde776cdc7f7
danil <d.shumskikh@gmail.com>
2020-02-04 21:23:59 +0300

    Каркас сайта

---
+++ gulpfile.js
@@ -0,0 +1,96 @@
+var gulp         = require('gulp'),
+		sass         = require('gulp-sass'),
+		browserSync  = require('browser-sync').create(),
+		concat       = require('gulp-concat'),
+		uglify       = require('gulp-uglify-es').default,
+		cleancss     = require('gulp-clean-css'),
+		rename       = require('gulp-rename'),
+		autoprefixer = require('gulp-autoprefixer'),
+		rsync        = require('gulp-rsync'),
+		newer        = require('gulp-newer'),
+		responsive   = require('gulp-responsive'),
+		del          = require('del');
+
+// Local Server
+gulp.task('browser-sync', function() {
+	browserSync.init({
+		server: {
+			baseDir: 'app'
+		},
+		notify: false,
+		// online: false, // Work offline without internet connection
+		// tunnel: true, tunnel: 'projectname', // Demonstration page: http://projectname.localtunnel.me
+	})
+});
+function bsReload(done) { browserSync.reload(); done() };
+
+// Custom Styles
+gulp.task('styles', function() {
+	return gulp.src('app/sass/**/*.sass')
+	.pipe(sass({
+		outputStyle: 'expanded',
+		includePaths: [__dirname + '/node_modules']
+	}))
+	.pipe(concat('styles.min.css'))
+	.pipe(autoprefixer({
+		// grid: true, // Optional. Enable CSS Grid
+		overrideBrowserslist: ['last 10 versions']
+	}))
+	.pipe(cleancss( {level: { 1: { specialComments: 0 } } })) // Optional. Comment out when debugging
+	.pipe(gulp.dest('app/css'))
+	.pipe(browserSync.stream())
+});
+
+// Scripts & JS Libraries
+gulp.task('scripts', function() {
+	return gulp.src([
+		'node_modules/jquery/dist/jquery.min.js', // Optional jQuery plug-in (npm i --save-dev jquery)
+		'app/js/_libs.js', // JS libraries (all in one)
+		'app/js/_custom.js', // Custom scripts. Always at the end
+		])
+	.pipe(concat('scripts.min.js'))
+	.pipe(uglify()) // Minify js (opt.)
+	.pipe(gulp.dest('app/js'))
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+// Responsive Images
+var quality = 95; // Responsive images quality
+
+gulp.task('img',bsReload);
+
+// Clean @*x IMG's
+gulp.task('cleanimg', function() {
+	return del(['app/img/@*'], { force: true })
+});
+
+// Code & Reload
+gulp.task('code', function() {
+	return gulp.src('app/**/*.html')
+	.pipe(browserSync.reload({ stream: true }))
+});
+
+// Deploy
+gulp.task('rsync', function() {
+	return gulp.src('app/')
+	.pipe(rsync({
+		root: 'app/',
+		hostname: 'username@yousite.com',
+		destination: 'yousite/public_html/',
+		// include: ['*.htaccess'], // Included files
+		exclude: ['**/Thumbs.db', '**/*.DS_Store'], // Excluded files
+		recursive: true,
+		archive: true,
+		silent: false,
+		compress: true
+	}))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('app/sass/**/*.sass', gulp.parallel('styles'));
+	gulp.watch(['app/js/_custom.js', 'app/js/_libs.js'], gulp.parallel('scripts'));
+	gulp.watch('app/*.html', gulp.parallel('code'));
+	gulp.watch('app/img/_src/**/*', gulp.parallel('img'));
+});
+
+gulp.task('default', gulp.parallel('img', 'styles', 'scripts', 'browser-sync', 'watch'));
